5c24ab3d624d04856a1a08422b3bc677912f3b9c
Ho2bes <8696@holbertonstudents.com>
2024-04-16 13:50:08 +0000

    modification of code add_wait_wait_execve.c

--- fork_wait_execve.c
+++ fork_wait_execve.c
@@ -3,31 +3,34 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-int main() {
-    int i;
+int main()
+{
+	printf("Parent process running...\n");
+	for (int child_num = 1; child_num <= 5; child_num++)
+	{
+		pid_t child_pid = fork();
 
-    printf("Parent process running...\n");
-    for (i = 0; i < 5; i++) {
-        pid_t pid = fork();
-
-        if (pid == -1) {
-            perror("Error:");
-            return 1;
-        } else if (pid == 0) {
-            // Child process
-            printf("Child process %d running...\n", i+1);
-            execlp("ls", "ls", "-l", "/tmp", NULL);
-            perror("Error:");
-            exit(EXIT_FAILURE);
-        } else {
-            // Parent process
-            int status;
-            wait(&status);
-            printf("Child process %d exited with status %d\n", i+1, WEXITSTATUS(status));
-        }
-    }
-
-    printf("All child processes have exited.\n");
-
-    return 0;
+		if (child_pid == -1) 
+		{
+			perror("Error:");
+			return 1;
+		}
+		else if (child_pid == 0)
+		{
+			// Child process
+			printf("Child process %d running...\n", child_num);
+			execlp("ls", "ls", "-l", "/tmp", NULL);
+			perror("Error:");
+			exit(EXIT_FAILURE);
+		}
+		else
+		{
+			// Parent process
+			int status;
+			wait(&status);
+			printf("Child process %d exited with status %d\n", child_num, WEXITSTATUS(status));
+		}
+	}
+	printf("All child processes have exited.\n");
+	return 0;
 }
