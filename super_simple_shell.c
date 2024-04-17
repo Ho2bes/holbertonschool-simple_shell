b8c1367966a1aad10706b99b79329c24581544f0
Ho2bes <8696@holbertonstudents.com>
2024-04-17 08:56:13 +0000

    modification of getline super_simple_shell.c

--- super_simple_shell.c
+++ super_simple_shell.c
@@ -6,15 +6,16 @@
 #define MAX_COMMAND_LENGTH 100 // Définition de la longueur maximale d'une commande
 
 int main() {
-    char user_command[MAX_COMMAND_LENGTH]; // Déclaration d'une variable pour stocker la commande de l'utilisateur
+    char *user_command = NULL; // Utilisation d'un pointeur de type char pour stocker la commande de l'utilisateur
+    size_t command_buffer_size = 0; // Taille initiale du tampon pour getline
 
     while (1) { // Boucle infinie pour l'exécution continue du shell
         // Afficher le prompt
         printf("#$ ");
 
-        // Lire la commande de l'utilisateur
-        if (fgets(user_command, sizeof(user_command), stdin) == NULL) {
-            break; // Sortir de la boucle si Ctrl+C est pressé ou si fgets échoue
+        // Lire la commande de l'utilisateur avec getline
+        if (getline(&user_command, &command_buffer_size, stdin) == -1) {
+            break; // Sortir de la boucle si Ctrl+C est pressé ou si getline échoue
         }
 
         // Supprimer le saut de ligne à la fin de la commande
@@ -39,5 +40,6 @@ int main() {
         }
     }
 
+    free(user_command); // Libérer la mémoire allouée par getline
     return 0; // Fin du programme
 }
