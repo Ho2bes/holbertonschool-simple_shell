858b20563526348b1b6dc1f03647f8543f891e5d
Grilindor <marcor91@hotmail.fr>
2024-04-23 13:21:53 +0000

    adding exe_comd.c for void execute_command, betty ok and work

---
+++ exe_comd.c
@@ -0,0 +1,52 @@
+#include "willson.h"
+
+/**
+ * execute_command - execut command from input
+ * @command: input command from the main
+ * @env: environment variables
+ * Return: Void
+ **/
+
+void execute_command(char *command, char **env)
+{/* Tokenizing the command */
+	char **command_tokens = tokenize_command(command);
+	char *command_path = find_command_path(command_tokens[0], env);
+/* Finding the full path of the command */
+	int i, status;
+
+	if (command_path == NULL)
+	{ /*If command not found,free memory and return*/
+		for (int i = 0; command_tokens[i] != NULL; i++)
+			free(command_tokens[i]);
+		free(command_tokens);
+		return;
+	}
+	pid_t pid = fork(); /* Creating a child process */
+
+	if (pid == -1)
+	{
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid == 0)
+	{ /* Child process */
+		if (execve(command_path, command_tokens, env) == -1)
+		{
+			perror("execve");
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{ /* Parent process and Freeing allocated memory*/
+		for (i = 0; command_tokens[i] != NULL; i++)
+			free(command_tokens[i]);
+		free(command_tokens);
+		free(command_path);
+
+		if (waitpid(pid, &status, 0) == -1)
+		{
+			perror("waitpid");
+			exit(EXIT_FAILURE);
+		}
+	}
+}
