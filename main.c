c84b24d434872eae30c7292ab9e5eb91874a4fb0
Ho2bes <8696@holbertonstudents.com>
2024-04-23 12:48:45 +0000

    add main.c file

---
+++ main.c
@@ -0,0 +1,46 @@
+#include "willson.h"
+
+/**
+ * main - Entry point of the program
+ * @argc: Number of arguments passed to the program
+ * @argv: Array of pointers to the arguments
+ * @env: Array of pointers to the environment variables
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char **argv, char **env)
+{
+	char *input = NULL;
+	size_t bufsize = 0;
+	ssize_t nb;
+
+	while (1)
+	{
+		/* If STDIN is a terminal, display the prompt */
+		if (isatty(STDIN_FILENO))
+		{
+			printf("$ ");
+			fflush(stdout);
+		}
+		nb = getline(&input, &bufsize, stdin);
+		if (nb == -1)
+		{
+			break;
+			/* Exit the loop if getline fails */
+		}
+		if (nb > 1 && input[nb - 1] == '\n')
+		{
+			input[nb - 1] = '\0';
+		}
+		if (strcmp(input, "exit") == 0)
+		{
+			break;
+			/* Exit the loop if the command is "exit" */
+		}
+		execute_command(input, env);
+	}
+	free(input);
+	/* Check if input is NULL before freeing it */
+	return (0);
+}
