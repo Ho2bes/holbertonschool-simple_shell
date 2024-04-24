4ced772650d585ac144a454796908e1a07515207
Ho2bes <8696@holbertonstudents.com>
2024-04-24 07:43:26 +0000

    add limits library

--- willson.h
+++ willson.h
@@ -13,6 +13,7 @@
 #include <sys/types.h>
 #include <errno.h>
 #include <sys/stat.h>
+#include <limits.h>
 
 char *getenv_var(const char *var_name, char **env);
 char *find_command_path(const char *command, char **env);
