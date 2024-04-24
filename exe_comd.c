6a01cc5c46db80d655178298887021cb50476932
Ho2bes <8696@holbertonstudents.com>
2024-04-24 08:11:04 +0000

    modification of env variable

--- exe_comd.c
+++ exe_comd.c
@@ -9,7 +9,7 @@ void execute_command(char *command, char **env)
 {
 	/* Tokenizing the command */
 	char **command_tokens = tokenize_command(command);
-	char *command_path = find_command_path(command_tokens[0], env);
+	char *command_path = find_command_path(command_tokens[0]);
 	/* Finding the full path of the command */
 	int i, status;
 	pid_t pid; /* Declaration moved outside of the if-else block*/
