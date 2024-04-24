f8901cd0b82e2ac02d3d3338418325219cece344
Ho2bes <8696@holbertonstudents.com>
2024-04-24 07:34:54 +0000

    modification exe_comd.c loop for and declaration variable

--- exe_comd.c
+++ exe_comd.c
@@ -1,28 +1,27 @@
 #include "willson.h"
-
 /**
- * execute_command - execut command from input
+ * execute_command - execute command from input
  * @command: input command from the main
  * @env: environment variables
  * Return: Void
  **/
-
 void execute_command(char *command, char **env)
-{/* Tokenizing the command */
+{
+	/* Tokenizing the command */
 	char **command_tokens = tokenize_command(command);
 	char *command_path = find_command_path(command_tokens[0], env);
-/* Finding the full path of the command */
+	/* Finding the full path of the command */
 	int i, status;
+	pid_t pid; /* Declaration moved outside of the if-else block*/
 
 	if (command_path == NULL)
-	{ /*If command not found,free memory and return*/
-		for (int i = 0; command_tokens[i] != NULL; i++)
+	{ /* If command not found, free memory and return */
+		for (i = 0; command_tokens[i] != NULL; i++)
 			free(command_tokens[i]);
 		free(command_tokens);
 		return;
 	}
-	pid_t pid = fork(); /* Creating a child process */
-
+	pid = fork(); /* Creating a child process */
 	if (pid == -1)
 	{
 		perror("fork");
@@ -37,12 +36,11 @@ void execute_command(char *command, char **env)
 		}
 	}
 	else
-	{ /* Parent process and Freeing allocated memory*/
+	{ /* Parent process and Freeing allocated memory */
 		for (i = 0; command_tokens[i] != NULL; i++)
 			free(command_tokens[i]);
 		free(command_tokens);
 		free(command_path);
-
 		if (waitpid(pid, &status, 0) == -1)
 		{
 			perror("waitpid");
