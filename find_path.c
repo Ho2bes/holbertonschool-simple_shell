3c76dfedc29e4066d99747b1a209f398072563c6
Ho2bes <8696@holbertonstudents.com>
2024-04-24 10:54:44 +0000

    correct leak memory

--- find_path.c
+++ find_path.c
@@ -8,41 +8,39 @@
 char *find_command_path(const char *command)
 {
 	char *search_path = strdup(getenv("PATH"));
-	/* Retrieve the PATH environment variable */
 	char *token = strtok(search_path, ":");
-	/* Split the environment variable into tokens */
-	char full_path[PATH_MAX]; /* Use PATH_MAX to define the max size of path */
-	char *result = NULL; /* Declare the result initialized to NULL */
+	char full_path[PATH_MAX];
+	char *result = NULL;
 	struct stat st;
-	/* Check if command contains an absolute path */
+
 	if (command[0] == '/')
-	{ /* Check if the file exists and is executable */
+	{
 		if (stat(command, &st) == 0 && st.st_mode & S_IXUSR)
 		{
-			return (strdup(command)); /* Return the found full path */
+			free(search_path); /* Libérer la mémoire avant de retou */
+			return (strdup(command));
 		}
 		else
 		{
 			fprintf(stderr, "Error: Command '%s' not found\n", command);
+			free(search_path); /* Libérer la mémoire avant de retour*/
 			return (NULL);
 		}
 	}
-	while (token != NULL)/* Traverse the directories in the PATH */
-	{ /* Construct the full path of the command */
+
+	while (token != NULL)
+	{
 		snprintf(full_path, sizeof(full_path), "%s/%s", token, command);
-		/* Check if the file exists and is executable */
 		if (stat(full_path, &st) == 0 && st.st_mode & S_IXUSR)
 		{
 			result = strdup(full_path);
-			/* Duplicate the found full path */
 			break;
 		}
 		token = strtok(NULL, ":");
-		/* Move to the next directory in the PATH */
 	}
-	/* If the command was not found, display an error */
+
+	free(search_path); /*Libérer la mémoire avant de retou */
 	if (result == NULL)
 		fprintf(stderr, "Error: Command '%s' not found\n", command);
-	free(search_path);
-	return (result); /* Return the full path of the command */
+	return (result);
 }
