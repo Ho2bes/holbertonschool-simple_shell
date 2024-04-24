c6f98c5c3090fd0978a3c47b209b06de0b2fe282
Ho2bes <8696@holbertonstudents.com>
2024-04-24 08:11:35 +0000

    modification of env variable and prototype

--- find_path.c
+++ find_path.c
@@ -6,45 +6,44 @@
  * @env: environment variables
  * Return: string of the path
  */
-char *find_command_path(const char *command, char **env)
+char *find_command_path(const char *command)
 {
-	char *search_path = strdup(getenv_var("PATH", env));
-/*Retrieve the PATH environment variable*/
+	char *search_path = strdup(getenv("PATH"));
+	/* Retrieve the PATH environment variable */
 	char *token = strtok(search_path, ":");
-/*Split the environment variable into tokens*/
-	char full_path[PATH_MAX]; /*Use PATH_MAX to define the max size of path*/
-	char *result = NULL; /*Declare the result initialized to NULL*/
+	/* Split the environment variable into tokens */
+	char full_path[PATH_MAX]; /* Use PATH_MAX to define the max size of path */
+	char *result = NULL; /* Declare the result initialized to NULL */
 	struct stat st;
-/*Check if command contains an absolute path*/
+	/* Check if command contains an absolute path */
 	if (command[0] == '/')
-	{ /*Check if the file exists and is executable*/
+	{ /* Check if the file exists and is executable */
 		if (stat(command, &st) == 0 && st.st_mode & S_IXUSR)
 		{
-			return (strdup(command)); /*Return the found full path*/
+			return strdup(command); /* Return the found full path */
 		}
 		else
 		{
 			fprintf(stderr, "Error: Command '%s' not found\n", command);
-			return (NULL);
+			return NULL;
 		}
 	}
-	while (token != NULL)/*Traverse the directories in the PATH*/
-	{ /*Construct the full path of the command*/
+	while (token != NULL)/* Traverse the directories in the PATH */
+	{ /* Construct the full path of the command */
 		snprintf(full_path, sizeof(full_path), "%s/%s", token, command);
-		/*Check if the file exists and is executable*/
+		/* Check if the file exists and is executable */
 		if (stat(full_path, &st) == 0 && st.st_mode & S_IXUSR)
 		{
 			result = strdup(full_path);
-/*Duplicate the found full path*/
+			/* Duplicate the found full path */
 			break;
 		}
 		token = strtok(NULL, ":");
-		/*Move to the next directory in the PATH*/
+		/* Move to the next directory in the PATH */
 	}
-/*If the command was not found, display an error*/
+	/* If the command was not found, display an error */
 	if (result == NULL)
-		fprintf(stderr, "Error: Command '%s' not found or
-			not executable\n", command);
+		fprintf(stderr, "Error: Command '%s' not found or not executable\n", command);
 	free(search_path);
-	return (result); /*Return the full path of the command*/
+	return result; /* Return the full path of the command */
 }
